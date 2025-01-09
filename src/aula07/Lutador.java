1841136d3c09608d6524798ac5ec10fb488a8677
unknown <daniel0107borges@gmail.com>
2025-01-08 23:07:49 -0300

    criacao classe lutador

---
+++ src/aula07/Lutador.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula07;
+
+/**
+ *
+ * @author Daniel Borges
+ */
+public class Lutador {
+    private String nome;
+    private String nacionalidade;
+    private float altura;
+    private int idade;
+    private float peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates; 
+    
+    //métodos personalizados 
+    public void apresentar(){
+        
+    }
+    public void status(){
+        
+    }
+    public void ganharLuta(){
+        
+    }
+    public void perderLuta(){
+        
+    }
+    public void empatarLuta(){
+        
+    }
+    
+    //metodos especiais
+
+    public Lutador(String no, String na,int id,float al, float pe,int vi, int de, int em) {
+        this.setNome(no);
+        this.setNacionalidade(na);
+        this.setIdade(id);
+        this.setAltura(al);
+        this.setPeso(pe);
+        this.setVitorias(vi);
+        this.setDerrotas(de);
+        this.setEmpates(em);
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        if(this.getPeso() < 52.2){
+            this.setCategoria("Invalido");   
+        }else if(this.getPeso()<=70.3){
+            this.setCategoria("Leve");
+        }
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+    
+}
